bbe617335cf5f6670bda7c8ea3e3f3f55ffd525d
jpnotleks <jpnotleks@gmail.com>
2023-04-11 21:45:23 +0200

    Added Google colab files and removed others

--- difEQ.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib
-
-fig=plt.figure()
-fig.suptitle("Crank-Nicolson Method for the Heat Equation")
-fig,(ax1, ax2) = plt.subplots(1, 2, sharey=True)
-fig.show()
--- songsterr.py
+++
@@ -1,8 +0,0 @@
-from graphics import *
-import numpy
-from numpy import *
-
-panel=GraphWin(1000,500,autoflush=False)
-
-
-
